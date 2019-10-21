ca0b7d3b37face185d7ef69590f4834f40163cac
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-10-22 01:28:33 +0900

    🤩(coding): state-practice

---
+++ src/practices/State.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class StatePractice extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: "REACT",
+            url1: "https://github.com",
+            url2: "https://twitter.com"
+        };
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.title}</h1>
+                <ul>
+                    <a href={this.state.url1}>
+                        <li>Git</li>
+                    </a>
+                    <a href={this.state.url2}>
+                        <li>Twitter</li>
+                    </a>
+                </ul>
+            </div>
+        );
+    }
+};
+
+export default StatePractice;
